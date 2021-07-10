3174c282e3283474e7539c19df332fd5fa0247f7
ArlindPestisha <arlind.pestisha@hyperisland.se>
2021-07-10 12:05:40 +0200

    dividing the app in components

--- part2/course-information/src/App.js
+++ part2/course-information/src/App.js
@@ -1,60 +1,62 @@
-const Course = ({ course }) => {
-  return (
-    <>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </>
-  );
-};
+import Course from "./components/Course";
 
-const Header = (props) => {
-  console.log(props);
-  return (
-    <>
-      <h1>{props.course}</h1>
-    </>
-  );
-};
+// const Course = ({ course }) => {
+//   return (
+//     <>
+//       <Header course={course.name} />
+//       <Content parts={course.parts} />
+//       <Total parts={course.parts} />
+//     </>
+//   );
+// };
 
-const Part = (props) => {
-  console.log(props);
-  return (
-    <>
-      <p>
-        {props.parts[0].name} {props.parts[0].exercises}
-      </p>
-      <p>
-        {props.parts[1].name} {props.parts[1].exercises}
-      </p>
-      <p>
-        {props.parts[2].name} {props.parts[2].exercises}
-      </p>
-    </>
-  );
-};
+// const Header = (props) => {
+//   console.log(props);
+//   return (
+//     <>
+//       <h1>{props.course}</h1>
+//     </>
+//   );
+// };
 
-const Content = (props) => {
-  console.log(props);
-  return (
-    <>
-      <Part parts={props.parts} />
-    </>
-  );
-};
+// const Part = (props) => {
+//   console.log(props);
+//   return (
+//     <>
+//       <p>
+//         {props.parts[0].name} {props.parts[0].exercises}
+//       </p>
+//       <p>
+//         {props.parts[1].name} {props.parts[1].exercises}
+//       </p>
+//       <p>
+//         {props.parts[2].name} {props.parts[2].exercises}
+//       </p>
+//     </>
+//   );
+// };
 
-const Total = (props) => {
-  return (
-    <>
-      <p>
-        Number of exercises{" "}
-        {props.parts[0].exercises +
-          props.parts[1].exercises +
-          props.parts[2].exercises}
-      </p>
-    </>
-  );
-};
+// const Content = (props) => {
+//   console.log(props);
+//   return (
+//     <>
+//       <Part parts={props.parts} />
+//     </>
+//   );
+// };
+
+// const Total = (props) => {
+//   return (
+//     <>
+//       <p>
+//         Number of exercises{" "}
+//         {props.parts[0].exercises +
+//           props.parts[1].exercises +
+//           props.parts[2].exercises}
+//       </p>
+//     </>
+//   );
+// };
 
 const App = () => {
   const course = {
---
+++ part2/course-information/src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = ({course}) => {
+  return (
+    <>
+      <p>{ course}</p>
+    </>
+   );
+}
+ 
+export default Header;

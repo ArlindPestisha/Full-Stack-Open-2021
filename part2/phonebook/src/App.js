7134449568bc52261bbb601d197c2c77a3fdc0dc
ArlindPestisha <arlind.pestisha@hyperisland.se>
2021-07-13 18:40:13 +0200

    done with step one

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,22 +1,44 @@
+/* eslint-disable no-restricted-globals */
 import React, { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
 
+  const person = persons;
+
+  const addNewName = (event) => {
+    event.preventDefault();
+    console.log("button clicked", event.target);
+  };
+
+  const handleNewName = (event) => {
+    console.log(event.target.value);
+    setNewName(event.target.value);
+  };
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addNewName}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNewName} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {/* <ul>
+        <li>
+          {persons.map((person) => {
+            return (person = { person });
+          })}
+        </li>
+      </ul> */}
+      <div>
+        {persons.map(person => <div>{ person.name}</div>)}
+        {newName}
+      </div>
     </div>
   );
 };

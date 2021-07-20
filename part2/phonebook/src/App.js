2f328fd30c624255b7cb3e8b5691bef6ed2dd6f1
ArlindPestisha <arlind.pestisha@hyperisland.se>
2021-07-20 18:00:23 +0200

    bug fixing

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,22 +4,40 @@ import React, { useState } from "react";
 const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
+  const [personExists, setPersonExists] = useState(true);
 
-  const person = persons;
+  // const person = persons;
 
   const addNewName = (event) => {
     event.preventDefault();
     console.log("button clicked", event.target);
+    const personObj = {
+      name: newName,
+      important: Math.random() < 0.5,
+      id: persons.length + 1,
+    };
+    setPersons(persons.concat(personObj));
+    setNewName("");
   };
 
   const handleNewName = (event) => {
     console.log(event.target.value);
     setNewName(event.target.value);
   };
+
+  const nameExists = personExists
+    ? newName : newName.filter((name) => name.important );
+  const setNameExists = () => setPersonExists(!personExists)
+    
+    
   return (
     <div>
       <h2>Phonebook</h2>
+      {/* <div>
+        { nameExists.alert(nameExists) }
+      </div> */}
       <form onSubmit={addNewName}>
+        { setNameExists.alert(`${newName} already exists`) }
         <div>
           name: <input value={newName} onChange={handleNewName} />
         </div>
@@ -36,7 +54,9 @@ const App = () => {
         </li>
       </ul> */}
       <div>
-        {persons.map(person => <div>{ person.name}</div>)}
+        {persons.map((person) => (
+          <div key={person.id}>{person.name}</div>
+        ))}
         {newName}
       </div>
     </div>

7e93d99cd53b7a058023529ddf769834cf7030e3
ArlindPestisha <arlind.pestisha@hyperisland.se>
2021-08-25 05:51:11 +0200

    done with loweCase and alert func

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,20 +1,47 @@
 /* eslint-disable no-restricted-globals */
 import React, { useState } from "react";
-
+const Person = props => {
+  return (
+    <div>
+      {props.name}
+    </div>
+  )
+}
 const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
   const [personExists, setPersonExists] = useState(true);
 
+  const names = persons.map(person => {
+    return person.name.toLocaleLowerCase()
+  })
+
+  const personsList = persons.map(person => {
+  return <Person key={person.name} name={person.name} />
+})
+
   const addNewName = (event) => {
     event.preventDefault();
-    const personObj = {
-      name: newName,
-      // important: Math.random() < 0.5,
-      // id: persons.length + 1,
-    };
+    // const personObj = {
+    //   name: newName,
+    //   // important: Math.random() < 0.5,
+    //   // id: persons.length + 1,
+    // };
+    if (names.includes(newName.toLocaleLowerCase())) {
+      alert(`${newName} is already added to the phonebook`)
+    } else {
+      const replacePerson = (equal, person1, person2) => {
+        return person1.toUpperCase() + person2.toLocaleLowerCase()
+      }
 
-    setPersons(persons.concat(personObj));
+      const NameCase = newName.replace(/\b([a-zA-Z])(\w+)/g, replacePerson)
+
+      const personObj = {
+        name: NameCase
+      }
+      setPersons([...persons].concat(personObj));
+    }
+    
     setNewName("");
   };
 
@@ -44,10 +71,11 @@ const App = () => {
         </li>
       </ul> */}
       <div>
-        {persons.map((person) => (
+        {/* {persons.map((person) => (
           <div key={person.id}>{person.name}</div>
         ))}
-        {newName}
+        {newName} */}
+        {personsList}
       </div>
     </div>
   );
